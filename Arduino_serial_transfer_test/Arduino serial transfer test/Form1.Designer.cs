8933243e150e869f0f34845009eb958d368bb0e6
Oskar <crazyminecuber@gmail.com>
2019-03-23 18:26:34 +0100

    Added C# program that will communicate to arduino via serial port.

---
+++ Arduino_serial_transfer_test/Arduino serial transfer test/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Arduino_serial_transfer_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbxDebug = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM3";
+            this.serialPort1.WriteBufferSize = 32;
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(238, 405);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbxDebug
+            // 
+            this.tbxDebug.Location = new System.Drawing.Point(29, 12);
+            this.tbxDebug.Multiline = true;
+            this.tbxDebug.Name = "tbxDebug";
+            this.tbxDebug.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbxDebug.Size = new System.Drawing.Size(515, 387);
+            this.tbxDebug.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 450);
+            this.Controls.Add(this.tbxDebug);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbxDebug;
+    }
+}
+
---
+++ Arduino_serial_transfer_test/Arduino serial transfer test/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Arduino_serial_transfer_test
+{
+    public partial class Form1 : Form
+    {
+       
+        string message = "";
+        string data = "0,0 2,1 0,2 0,3 0,4 0,5 0,6 0,7";
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.Open();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string sendpacket = "";
+            int n = 0;
+            while(n < data.Length)
+            {
+                if(data[n] == ' ')
+                {
+                    debug(n.ToString());
+                    sendpacket += data[n];
+                    serialPort1.Write("1");
+                    string svar = "";
+                    while ((svar = readSerial()) == null) { }
+                    debug("Got an answer");
+                    serialPort1.Write(sendpacket);
+                    sendpacket = "";
+                }
+                else
+                {
+                    sendpacket += data[n];
+                }
+                n++;
+            }
+            serialPort1.Write("0");
+
+        }
+
+        void debug(string text)
+        {
+            text = "Dator: " + text + "\n";
+            tbxDebug.AppendText(text);
+            File.AppendAllText(Path.Combine("test.txt"), text);
+            this.Update();
+            this.Refresh();
+        }
+
+        string readSerial()
+        {
+            return message;
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            string temp = serialPort1.ReadLine();
+            File.AppendAllText(Path.Combine("test.txt"), "\r\n" + serialPort1.BytesToRead.ToString() + "\r\n");
+            char lead = temp[0];
+            temp = temp.Substring(1);
+            if (lead == 'G')
+            {
+                File.AppendAllText(Path.Combine("test.txt"), temp + "\n");
+                tbxDebug.Invoke(new Action(() => tbxDebug.AppendText("Arduino: " + temp + "\n")));
+                this.Invoke(new Action(() => this.Update()));
+                this.Invoke(new Action(() => this.Refresh()));
+                message = null;
+            }
+            else if (lead == 'A')
+            {
+                message =  temp;
+            }
+            else
+            {
+                string text = "Felaktig lead: " + lead + temp + "\n";
+                File.AppendAllText(Path.Combine("test.txt"), text);
+                tbxDebug.Invoke(new Action(() => tbxDebug.AppendText(text)));
+                this.Invoke(new Action(() => this.Update()));
+                this.Invoke(new Action(() => this.Refresh()));
+                message = null;
+            }
+
+        }
+    }
+}
+
+
